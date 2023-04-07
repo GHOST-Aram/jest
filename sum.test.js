f1fbb780b41041ff7542d7c6d1b84ab6a5e389ea
GHOST-Aram <owinoojala@gmail.com>
2023-04-07 14:39:07 +0300

    chore: Move Cypher Class test code to its own file

--- sum.test.js
+++ sum.test.js
@@ -30,40 +30,4 @@ test('Assign Property and Values: With array referencing ', () =>{
     expect('Life').toBe('Life')
 })
 //Text Cypher
-const cypher = new Cypher()
-
-test('Encrypt One word: ',()=>{
-    const word = 'school'
-    expect(cypher.ceasarCypher(word,1)).toBe('tdippm')
-})
-
-test('Encrypt two words separated with space: ',()=>{
-    const word = 'school boy'
-    expect(cypher.ceasarCypher(word,1)).toBe('tdippm cpz')
-})
-
-test('Encrypt  Long Sentense: ',()=>{
-    const word = 'school boy needs to be obidient'
-    expect(cypher.ceasarCypher(word,1)).toBe('tdippm cpz offet up cf pcjejfou')
-})
-
-test('Should work for mixed upper and lower case: ',()=>{
-    const word = 'IBM computers and NASA'
-    expect(cypher.ceasarCypher(word,1)).toBe('JCN dpnqvufst boe OBTB')
-})
-
-test('Should work for Punctuated sentences: ',()=>{
-    const word = 'What!? I really, really hope you are safe.'
-    expect(cypher.ceasarCypher(word,1)).toBe('Xibu!? J sfbmmz, sfbmmz ipqf zpv bsf tbgf.')
-})
-
-test('Should circulate the alphabet from z back to a: ',()=>{
-    const word = 'Zebras'
-    expect(cypher.ceasarCypher(word,1)).toBe('Afcsbt')
-})
-
-test('Should work for wide shift ',()=>{
-    const word = 'Zebras'
-    expect(cypher.ceasarCypher(word,12)).toBe('Lqndme')
-})
 
